716b159bd0b3899543e0a91b66910fbca733d8c3
Uri <orioljg2002@gmail.com>
2024-11-21 18:48:05 +0100

    Actualitzat

--- Exam06/mini_serv.c
+++ Exam06/mini_serv.c
@@ -4,19 +4,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include <netinet/in.h>
 #include <sys/select.h>
+#include <netinet/in.h>
 
-typedef struct s_client
+typedef struct client_s
 {
     int id;
-    char msg[10000];
-}   t_client;
+    char msg[100000];
+}   client_t;
 
-t_client clients[1024];
-fd_set read_set, write_set, current;
+client_t clients[1024];
 int maxfd = 0, cli_id = 0;
-char send_buffer[10000], recv_buffer[10000];
+fd_set current, read_set, write_set;
+char send_buffer[100000], recv_buffer[100000];
 
 void printError(const char *msg)
 {
@@ -24,7 +24,7 @@ void printError(const char *msg)
         write(2, msg, strlen(msg));
     else
         write(2, "Fatal error\n", 12);
-    exit(1);
+    exit (1);
 }
 
 void broadcast_message(int sender)
@@ -35,31 +35,11 @@ void broadcast_message(int sender)
                 printError(NULL);
 }
 
-void process_message(int fd)
-{
-    int i = 0;
-    while (i < strlen(recv_buffer))
-    {
-        int j = i;
-        while (recv_buffer[j] != '\n' && recv_buffer[j] != '\0')
-            j++;
-        if (recv_buffer[j] == '\n')
-        {
-            recv_buffer[j] = '\0';
-            sprintf(send_buffer, "client %d: %s\n", clients[fd].id, &recv_buffer[i]);
-            broadcast_message(fd);
-            i = j + 1;
-        }
-        else
-            break ;
-    }
-}
-
 int main(int argc, char *argv[])
 {
     if (argc != 2)
         printError("Wrong number of arguments\n");
-    
+
     struct sockaddr_in serveraddr;
     socklen_t len = sizeof(serveraddr);
     int serverfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -69,7 +49,7 @@ int main(int argc, char *argv[])
 
     memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serveraddr.sin_port = htons(atoi(argv[1]));
     if (bind(serverfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1)
         printError(NULL);
@@ -110,12 +90,23 @@ int main(int argc, char *argv[])
                         broadcast_message(fd);
                         FD_CLR(fd, &current);
                         close(fd);
-                        clients[fd].msg[0] = '\0';
+                        bzero(clients[fd].msg, strlen(clients[fd].msg));
                     }
                     else
                     {
                         recv_buffer[bytes] = '\0';
-                        process_message(fd);
+                        for (int i = 0, j = strlen(clients[fd].msg); i < bytes; i++, j++)
+                        {
+                            clients[fd].msg[j] = recv_buffer[i];
+                            if (clients[fd].msg[j] == '\n')
+                            {
+                                clients[fd].msg[j] = '\0';
+                                sprintf(send_buffer, "client %d: %s\n", clients[fd].id, clients[fd].msg);
+                                broadcast_message(fd);
+                                bzero(clients[fd].msg, strlen(clients[fd].msg));
+                                j = -1;
+                            }
+                        }
                     }
                 }
             }
@@ -123,3 +114,4 @@ int main(int argc, char *argv[])
     }
     return (0);
 }
+
